50f4be526dac32b7b04dddfee917d907dc114410
zyh <2806117849@qq.com>
2026-08-16 20:22:31 +0800

    remove .continue from tracking; add .gitignore; update sockutil

--- include/sockutil.h
+++ include/sockutil.h
@@ -3,8 +3,8 @@
 
 #include"server.h"
 int create_socket(int domain,int type,int protocol);
-int bind_socket(int sockfd,const struct sockaddr* addr,socklen_t addrlen);
-int set_socket_nonblocking(int sockfd);
+int bind_socket(int domain,int sockfd,int port=8888);
+void set_socket_nonblocking(int sockfd);
 int set_socket_reuseaddr(int sockfd);
 int listen_socket(int sockfd,int backlog=128);
 
@@ -12,6 +12,3 @@ ssize_t Read(int fd,void* buf,size_t len);
 ssize_t Write(int fd,const void* buf,size_t len);
 
 #endif //_SOCKUTIL_H
-/*
-sk-cae9a4a903c74e18bd6e3c12b28a5500
-*/
--- src/sockutil.cpp
+++ src/sockutil.cpp
@@ -1,16 +1,54 @@
-#include"sockutil.h"
+#include "sockutil.h"
 
-int create_socket(int domain,int type,int protocol)
+int create_socket(int domain, int type, int protocol)
 {
-    int sockfd=socket(domain,type,protocol);
-    if(sockfd<0){
+    int sockfd = socket(domain, type, protocol);
+    if (sockfd < 0)
+    {
         perror("socket error");
         exit(EXIT_FAILURE);
     }
     return sockfd;
-}   
+}
 
-int bind_socket(int sockfd,const struct sockaddr* addr,socklen_t len)
+int bind_socket(int domain,int sockfd,int port=8888)
 {
+    struct sockaddr_in addr;
+    addr.sin_addr.s_addr=htonl(INADDR_ANY);
+    addr.sin_family=domain;
+    addr.sin_port=port;
+    int ret=bind(sockfd,(struct sockaddr*)&addr,sizeof(addr));
+    return ret;
+}
 
+void set_socket_nonblocking(int sockfd)
+{
+    int flags = fcntl(sockfd, F_GETFL);
+    flags |= O_NONBLOCK;
+    fcntl(sockfd,F_SETFL,flags);
+}
+
+int set_socket_reuseaddr(int sockfd)
+{
+    int opt=1;
+    int ret;
+    ret=setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    return ret;
+}
+
+ssize_t Read(int fd,void* buf,size_t len){
+    size_t n;
+    do{
+        n=read(fd, buf, len);
+    }while(errno==EINTR);
+    if(n<0){
+        perror("read error");
+        return n;
+    }
+    else if(n==0){
+        return n;
+    }
+    else{
+        return n;
+    }
 }
